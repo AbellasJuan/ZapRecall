018bd64088a83154dadcc2f9eb8401438feacdcb
AbellasJuan <juanvictoraa@gmail.com>
2021-12-12 23:32:57 -0300

    bonus 'try again'

--- src/components/Failure.js
+++ src/components/Failure.js
@@ -1,10 +1,24 @@
+import { useState } from "react"
+import Home from "../components/Home.js"
+
 export default function Failure(){
+
+    const [tentarNovamente, setTentarNovamente] = useState('parabens');
+
     return(
+        <>
+        {tentarNovamente === 'parabens' ? 
+        <>
         <div className="result-screen">
             <span>Putz..
                 <img src="../assets/sad.png" alt=""/>    
             </span> 
             <p>Você esqueceu alguns flashcards..<br/>Não desanime! Na próxima você consegue!</p>
         </div>
+            <button className="praticar-react exemplo" onClick={() => setTentarNovamente('home')}>
+            <span>Tentar novamente</span>
+            </button> 
+        </> : <Home/>} 
+        </>
     )
 }
--- src/components/Success.js
+++ src/components/Success.js
@@ -1,6 +1,14 @@
+import { useState } from "react"
+import Home from "../components/Home.js"
+
 export default function Success(){
+
+    const [tentarNovamente, setTentarNovamente] = useState('parabens');
+
     return(
-        
+        <>
+    {tentarNovamente === 'parabens' ? 
+    <>
         <div className="result-screen">
             <span>PARABÉNS!
                 <img src="assets/party.png" alt=""/>    
@@ -9,5 +17,10 @@ export default function Success(){
         
         <p>Você não esqueceu de nenhum flashcard!</p>
         </div>
+        <button className="praticar-react exemplo" onClick={() => setTentarNovamente('home')}>
+        <span>Tentar novamente</span><img src="../assets/next.png" alt=""/>
+        </button> 
+    </> : <Home/> } 
+    </>
     )
 }
