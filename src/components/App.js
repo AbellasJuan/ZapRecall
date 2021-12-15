c35948240ce6bd04d1ea5585ab3bb2341f38d92b
thiagoribb <thiribeiro142@gmail.com>
2021-12-15 16:47:19 -0300

    Preparando revisão de código

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import Home from "../components/Home.js";
-import "../css/style.css";
-
-export default function App(){
-
-    return (
-        <Home/>
-    )
-};
--- src/components/Header.js
+++
@@ -1,5 +0,0 @@
-export default function Header(){
-    return(
-        <img className="logo-mini-top" src="assets/logo-mini.png" alt=""/>
-    )
-};
