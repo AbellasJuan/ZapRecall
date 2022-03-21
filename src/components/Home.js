69706e864e27af60f08b96ad8c9a47fb9dec7ae9
abellasjuan <juanvictoraa@gmail.com>
2022-03-21 12:40:40 -0300

    bug fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -61,14 +61,14 @@ const [paginaAtual, setPaginaAtual] = useState("Home");
         {paginaAtual === "Home" ?
         <div  className="homepage">
         
-        <div>
-        <img className="initial-main-img" src="../assets/logo.png" alt=""></img>
-        </div>
+            <div>
+                <img className="initial-main-img" src="../assets/logo.png" alt=""></img>
+            </div>
 
-        <button className="praticar-react" onClick={() => setPaginaAtual("FlashCardFront")} data-identifier="start-zap-recall">
-            <span>Praticar React  </span>
-            <img src="../assets/next.png" alt=""/>
-        </button> 
+            <button className="praticar-react" onClick={() => setPaginaAtual("FlashCardFront")} data-identifier="start-zap-recall">
+                <span>Praticar React  </span>
+                <img src="../assets/next.png" alt=""/>
+            </button> 
         
         </div> : <FlashCardFront acertouMaisUm={acertouMaisUm} acertou={acertou} questions={questions} contadorCarta={contadorCarta} />}
     </>
